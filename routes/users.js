c4c56826e3301d3ccd12253012912b7ea64d82b0
jayant pal <jpofficials7@gmail.com>
2023-08-26 13:06:32 +0530

    users routes

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ import express from "express";
 import { v4 as uuidv4 } from "uuid";
 const router = express.Router();
 
-const users = [];
+let users = [];
 
 router.get("/", (req, res) => {
   console.log(users);
@@ -18,8 +18,25 @@ router.post("/", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-  console.log("query", req.query);
-  res.send(req.params);
+  const { id } = req.params;
+  const foundUser = users.find((user) => user.id === id);
+  res.send(foundUser);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  users = users.filter((user) => user.id !== id);
+  res.send(`User with the id ${id} is deleted from the database`);
+});
+
+router.patch("/:id", (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName, age } = req.body;
+  const user = users.find((user) => user.id === id);
+  if (firstName) user.firstName = firstName;
+  if (lastName) user.lastName = lastName;
+  if (age) user.age = age;
+  res.send(`User with the id ${id} has been updated.`);
 });
 
 export default router;
